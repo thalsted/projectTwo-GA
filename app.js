4503e1d33a7be1bf5666273a3b3ae6e12e13e6a5
Taylor Halsted <taylor.halsted@gmail.com>
2016-11-26 11:16:22 -0500

    add more pages

--- app.js
+++ app.js
@@ -28,3 +28,15 @@ app.get('/', function(req,res) {
 app.get('/login', function(req,res) {
   res.render('login')
 })
+
+app.post('/login', function(req,res) {
+
+})
+
+app.get('/signup', function(req,res) {
+  res.render('signup')
+})
+
+app.get('/account', function(req,res) {
+  res.render('account')
+})
