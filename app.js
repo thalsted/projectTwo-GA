3020a0bf0329ad675bcb908f764613b8d52dd166
Taylor Halsted <taylor.halsted@gmail.com>
2016-11-28 18:09:07 -0500

    add monday work

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-var express = require('express');
-var app = express();
-var pgp = require('pg-promise')();
-var db = pgp('postgres://taylorhalsted@localhost:5432/hustlepuffin');
-// pgp(process.env.DATABASE_URL) || not working ##Need to debug
-var mustache = require('mustache-express');
-var methodOverride = require('method-override');
-var bdPars = require('body-parser');
-var fetch = require('node-fetch');
-
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log('Server alive on port 3000!');
-});
+const express = require('express');
+const app = express();
+const pgp = require('pg-promise')();
+const db = pgp(process.env.DATABASE_URL || 'postgres://taylorhalsted@localhost:5432/hustlepuffin');
+const mustache = require('mustache-express');
+const methodOverride = require('method-override');
+const bdPars = require('body-parser');
+const fetch = require('node-fetch');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
 
 app.engine('html',mustache());
 app.set('view engine','html');
@@ -20,6 +16,13 @@ app.use(express.static(__dirname+'/public'));
 app.use(methodOverride('_method'));
 app.use(bdPars.urlencoded({ extended: false}));
 app.use(bdPars.json());
+
+app.use(session({
+  secret: 'ForeverPuff',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 
 app.get('/', function(req,res) {
   res.render('index')
@@ -30,17 +33,131 @@ app.get('/login', function(req,res) {
 })
 
 app.post('/login', function(req,res) {
-
+  var data = req.body;
+  db.one(
+    "SELECT * FROM users WHERE email = $1",
+    [data.email]
+  ).catch(function(){
+    res.render('woops');
+  }).then(function(user){
+    bcrypt.compare(data.password, user.password_digest, function(err, cmp){
+      if(cmp) {
+        req.session.user = user;
+        res.redirect('/account');
+      } else {
+        res.render('woops');
+      }
+    })
+  })
 })
 
 app.get('/signup', function(req,res) {
   res.render('signup')
 })
 
+app.post('/signup', function(req,res) {
+  var data = req.body;
+
+  bcrypt.hash(data.password, 10, function(err, hash){
+    db.none(
+      "INSERT INTO users(email, password_digest) VALUES($1, $2)", [data.email, hash]
+    ).catch(function(){
+      res.render('woops');
+    }).then(function(){
+      req.session.user = data;
+      res.redirect('/account')
+    })
+  })
+})
+
 app.get('/account', function(req,res) {
+  var logged_in;
+  var email;
+
+  if(req.session.user){
+    logged_in = true;
+    email = req.session.user.email;
+  }
+
+  var data = {
+   "logged_in": logged_in,
+   "email": email
+  }
+
   res.render('account')
 })
 
 app.get('/companies', function(req,res) {
+  var logged_in;
+  var email;
+
+  if(req.session.user){
+    logged_in = true;
+    email = req.session.user.email;
+  }
+
+  var data = {
+   "logged_in": logged_in,
+   "email": email
+  }
+
+  db.many("SELECT * FROM companies WHERE user_email = $1",[data.email])
+  .catch(function(err){
+    console.log(err)
+  })
+  .then(function(cData) {
+    var json_data = {companies: cData}
+    console.log(json_data)
+    res.render('companies', json_data)
+  })
+})
+
+app.post('/companies', function(req,res) {
+  var logged_in;
+  var email;
+
+  if(req.session.user){
+    logged_in = true;
+    email = req.session.user.email;
+  }
+
+  var data = {
+   "logged_in": logged_in,
+   "email": email
+  }
+
+  var input = req.body;
+
+  db.none("INSERT INTO companies(name, phase, industry, description, url, user_email) VALUES($1, $2, $3, $4, $5, $6)",[input.company, input.phase, input.industry, input.desc, input.url, data.email])
+  .then(function(){
+    res.redirect('/companies')
+  })
+})
+
+app.get('/contacts/:id', function(req,res) {
+  var logged_in;
+  var email;
+  var id = req.params.id;
+
+  if(req.session.user){
+    logged_in = true;
+    email = req.session.user.email;
+  }
+
+  var data = {
+   "logged_in": logged_in,
+   "email": email
+  }
+
+  db.many("SELECT * FROM contacts JOIN companies ON contacts.company_id = companies.id WHERE companies.user_email = $1 AND contacts.company_id = $2",[data.email,id])
+  .then(function(data) {
+    console.log(data)
+  })
+
   res.render('companies')
 })
+
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Server alive on port 3000!');
+});
--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,29 @@
-DROP TABLE IF EXISTS companies;
-DROP TABLE IF EXISTS contacts;
-DROP TABLE IF EXISTS interactions;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS companies CASCADE;
+DROP TABLE IF EXISTS contacts CASCADE;
+DROP TABLE IF EXISTS interactions CASCADE;
+
+CREATE TABLE users (
+  user_id SERIAL,
+  email VARCHAR(255) UNIQUE,
+  password_digest VARCHAR(255),
+  PRIMARY KEY (user_id)
+);
 
 CREATE TABLE companies(
-  id SERIAL,
+  comp_id SERIAL,
   name VARCHAR(50) NOT NULL,
   phase VARCHAR(15) CHECK (phase IN ('Target', 'Engaged', 'Interviewing','Lost','Won')) NOT NULL,
   industry VARCHAR(25),
   description VARCHAR(300),
-  url VARCHAR(100),
-  PRIMARY KEY (id)
+  url VARCHAR(200),
+  user_email VARCHAR(255),
+  PRIMARY KEY (comp_id),
+  FOREIGN KEY (user_email) REFERENCES users(email)
 );
 
 CREATE TABLE contacts(
-  id SERIAL,
+  cont_id SERIAL,
   contact_name VARCHAR(50) NOT NULL,
   company_id INTEGER NOT NULL,
   title VARCHAR(50),
@@ -22,12 +32,12 @@ CREATE TABLE contacts(
   found_through VARCHAR(50),
   date_created DATE NOT NULL DEFAULT(CURRENT_DATE),
   note VARCHAR(200),
-  PRIMARY KEY (id),
-  FOREIGN KEY (company_id) REFERENCES companies(id)
+  PRIMARY KEY (cont_id),
+  FOREIGN KEY (company_id) REFERENCES companies(comp_id)
 );
 
 CREATE TABLE interactions(
-  id SERIAL,
+  int_id SERIAL,
   contact_id INTEGER NOT NULL,
   interaction_date DATE NOT NULL,
   medium VARCHAR(25),
@@ -37,6 +47,6 @@ CREATE TABLE interactions(
   next_step VARCHAR(100),
   next_date DATE,
   score INTEGER CHECK (score > 0 AND score < 11),
-  PRIMARY KEY (id),
-  FOREIGN KEY (contact_id) REFERENCES contacts(id)
+  PRIMARY KEY (int_id),
+  FOREIGN KEY (contact_id) REFERENCES contacts(cont_id)
 );
